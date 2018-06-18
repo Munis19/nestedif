361fc00cc4c88953cd855ed3c677c26e752ac4f5
Munis  Chandru <30998895+Munis19@users.noreply.github.com>
2018-06-18 11:23:29 +0530

    Add files via upload

---
+++ nestedif.java
@@ -0,0 +1,32 @@
+class nestedif
+{
+public static void main(String arg[])
+{
+int a=12,b=10,c=15;
+if(a<b)
+{
+if(a<c)
+{
+System.out.println("a is the smallest number");
+}
+else
+{
+System.out.println("c is the smallest number");
+}
+}
+else
+{
+if(b<c)
+{
+System.out.println("b is the smallest number");
+}
+else
+{
+System.out.println("c is the smallest number");
+}
+}
+}
+}
+
+
+
